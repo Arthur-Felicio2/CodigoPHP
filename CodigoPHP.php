4ecc57a23ca2a1f2bebf60b6f10a69e82238fc31
Arthur-Felicio2 <arthur.felicio@unesp.br>
2025-03-13 07:39:55 -0300

    Update CodigoPHP.php

--- CodigoPHP.php
+++ CodigoPHP.php
@@ -1,3 +1,9 @@
 <?php
-    echo "hello World";
+//declaração das variaveis
+$city = "Bauru ";
+$day = 6 ;
+$month =  " Março";
+$year = 2025;
+echo "$city , $day de $month de $year"; /*Output do Codigo, de forma simples,
+ o codigo irá imprimir os valores dentro das variaveis*/
 ?>
